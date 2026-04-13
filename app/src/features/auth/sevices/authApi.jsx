3fdb5010f9bf43aa851a45b568bc611ecee6927b
Juan Benitez <juanpenagos007@gmail.com>
2026-04-13 06:31:08 -0500

    Fix conexión backend con Mongo Atlas y puertos

--- app/src/features/auth/sevices/authApi.jsx
+++ app/src/features/auth/sevices/authApi.jsx
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_URL = 'http://localhost:3000/api/auth'
+const API_URL = 'https://taller4-backend.onrender.com/api/gastos'
 
 export const registerUser =(data) =>
     axios.post(`${API_URL}/register`,data)
